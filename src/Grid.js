078f1f6d3cfa5e86a550396b2d3f5ced53c182ec
alieliacik <ali.eliacik@hotmail.com>
2021-03-06 12:04:52 -0500

    grid naming

--- src/Grid.js
+++ src/Grid.js
@@ -3,24 +3,47 @@ import styled from 'styled-components'
 const StyledGrid = styled.div`
   display: grid;
   grid-gap: 20px;
+  grid-template-rows: [header-start] 100px [header-end box-start] 200px [box-end main-start] 400px [main-end footer-start] 100px [footer-end];
+  grid-template-columns: repeat(3, [col-start] 1fr [col-end]) 200px [grid-end];
+
   height: 80vh;
-  border: 1px solid black;
+  padding: 15px;
 
   & > * {
     background-color: red;
+    display: grid;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    font-weight: bold;
+  }
+
+  .header {
+    grid-column: col-start 1 / grid-end;
+  }
+
+  .sidebar {
+    grid-column: 4 / -1;
+    grid-row: box-start / main-end;
+  }
+  .main {
+    grid-column: 1 / 4;
+  }
+  .footer {
+    grid-column: 1 / -1;
   }
 `
 
 const Grid = () => {
   return (
     <StyledGrid>
-      <div className='header'></div>
-      <div className='box1'></div>
-      <div className='box2'></div>
-      <div className='box3'></div>
-      <div className='sidebar'></div>
-      <div className='main'></div>
-      <div className='footer'></div>
+      <div className='header'>Header</div>
+      <div className='box1'>Box</div>
+      <div className='box2'>Box</div>
+      <div className='box3'>Box</div>
+      <div className='sidebar'>Sidebar</div>
+      <div className='main'>Main</div>
+      <div className='footer'>Footer</div>
     </StyledGrid>
   )
 }
